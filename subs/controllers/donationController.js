78760ed68869e89b8f393044f7197b58651a50c8
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-01-09 19:34:40 +0900

    create controllers

---
+++ subs/controllers/donationController.js
@@ -0,0 +1,18 @@
+class DonationController {
+    static async createPayment(req, res, next) {
+        try {
+
+        } catch (error) {
+            console.log(error, 'error create payment')
+        }
+    }
+
+    static async createSubscription(req, res, next) {
+        try {
+
+        } catch (error) {
+            console.log(error, 'error create subscription')
+        }
+    }
+}
+module.exports = DonationController
