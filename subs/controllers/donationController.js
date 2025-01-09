557868046d877f419395194a55a93128b1ffa607
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-01-09 20:04:09 +0900

    create subscription controller

--- subs/controllers/donationController.js
+++ subs/controllers/donationController.js
@@ -1,53 +1,64 @@
-const { v4: uuidv4 } = require('uuid')
-const midtransClient = require('midtrans-client')
-
+const { v4: uuidv4 } = require("uuid");
+const midtransClient = require("midtrans-client");
+const axios = require("axios");
 class DonationController {
     static async createPayment(req, res, next) {
         try {
-            const { email, amount, currency } = req.body
-            const transactionId = uuidv4()
+            const { email, amount, currency } = req.body;
+            const transactionId = uuidv4();
 
             let snap = new midtransClient.Snap({
                 isProduction: false,
                 serverKey: process.env.MID_TRANS_SERVER_KEY,
-            })
+            });
 
             let parameter = {
                 "transaction_details": {
                     "order_id": transactionId,
-                    "gross_amount": amount
+                    "gross_amount": amount,
                 },
                 "credit_card": {
-                    "secure": true
+                    "secure": true,
                 },
                 item_details: [
                     {
-                        id: 'donation',
+                        id: "donation",
                         price: amount,
                         quantity: 1,
+                    },
+                ],
+            };
 
-                    }
-                ]
-            }
-
-            const midTransToken = await snap.createTransaction(parameter)
+            const midTransToken = await snap.createTransaction(parameter);
 
             res.status(201).json({
                 ...midTransToken,
-                display_amount: amount
-            })
-
+                display_amount: amount,
+            });
         } catch (error) {
-            console.log(error, 'error create payment')
+            console.log(error, "error create payment");
         }
     }
 
     static async createSubscription(req, res, next) {
         try {
-
+            const { name, email, phone } = req.body
+            const options = {
+                method: "POST",
+                url: "",
+                headers: {
+                    accept: "application/json",
+                    'content-type': 'application/json'
+                },
+                data: {
+                    customer_details: {
+                        first_name: ''
+                    }
+                }
+            };
         } catch (error) {
-            console.log(error, 'error create subscription')
+            console.log(error, "error create subscription");
         }
     }
 }
-module.exports = DonationController
+module.exports = DonationController;
